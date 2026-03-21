56c2e155d1433bfaefdb01b664fa310125ad58f9
Prashant Masule <prashantmasule@gmail.com>
2026-03-21 12:32:16 +0530

    Refactor signingConfig logic in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -123,6 +123,8 @@ android {
             signingConfig =
                 if (workflowDebugKeystoreFile != null) {
                     signingConfigs.getByName("workflowDebug")
+                } else if (persistentDebugKeystoreFile.exists()) {
+                    signingConfigs.getByName("persistentDebug")
                 } else {
                     signingConfigs.getByName("debug")
                 }
